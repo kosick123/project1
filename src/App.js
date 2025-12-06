e57b069e10db18585825ad28610c91decd87a456
kosick123 <kousickpaulkareti@gmail.com>
2025-12-06 16:08:09 +0530

    bbuad

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 // Import all 10 pages
 import Page1 from "./Pages/Page1";
