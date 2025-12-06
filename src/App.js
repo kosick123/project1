eabd7315086f8c1523885562f9f2cb14e00c32ff
kosick123 <kousickpaulkareti@gmail.com>
2025-12-06 16:05:30 +0530

    niis

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 // Import all 10 pages
-import Page1 from "./pages/Page1";
-import Page2 from "./pages/Page2";
-import Page3 from "./pages/Page3";
-import Page4 from "./pages/Page4";
-import Page5 from "./pages/Page5";
-import Page6 from "./pages/Page6";
-import Page7 from "./pages/Page7";
-import Page8 from "./pages/Page8";
-import Page9 from "./pages/Page9";
-import Page10 from "./pages/Page10";
+import Page1 from "./Pages/Page1";
+import Page2 from "./Pages/Page2";
+import Page3 from "./Pages/Page3";
+import Page4 from "./Pages/Page4";
+import Page5 from "./Pages/Page5";
+import Page6 from "./Pages/Page6";
+import Page7 from "./Pages/Page7";
+import Page8 from "./Pages/Page8";
+import Page9 from "./Pages/Page9";
+import Page10 from "./Pages/Page10";
 
 function App() {
   return (
